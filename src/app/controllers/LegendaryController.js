1ac56db58e95d34c5919531d6685769a67f049a5
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-06-27 20:31:14 -0300

    inicializando projeto

---
+++ src/app/controllers/LegendaryController.js
@@ -0,0 +1,17 @@
+const LegendaryModel = require('../models/LegendaryModel');
+
+
+const controller = {
+    index: (request, response) => {
+        const pokemon = new LegendaryModel(
+            1,
+            "MewTwo",
+            "Descrição",
+            "Pokemon"
+        );
+
+        response.json(pokemon)
+    }
+}
+
+module.exports = controller;
